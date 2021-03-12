d33239c477ce6ee03eeec3a37f2010a4ef46babf
Hiramangp <38985888+Hiramangp@users.noreply.github.com>
2021-03-12 19:32:50 +0530

    Update ValuesController.cs

--- aws_demo/Controllers/ValuesController.cs
+++ aws_demo/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace aws_demo.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return "hell55 ?www ";
+            return "hello ?www ";
         }
         // GET api/values/5
         [HttpGet("{id}")]
